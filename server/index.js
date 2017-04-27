2d370f6810bfd5c5e27fd36a70be59adf6c7f108
Hack Reactor Student <student@hackreactor.com>
2017-04-27 11:19:24 -0700

    Update to prepare for heroku deploy

--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ app.get('/repos', function (req, res) {
   });
 });
 
-var port = 1128;
+var port = process.env.PORT || 1128;
 
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
